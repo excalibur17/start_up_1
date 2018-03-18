283362f30856226e681dc114b91b80c82aa02b97
excalibur17 <2976084714@qq.com>
2018-03-19 00:04:47 +0800

    cc

---
+++ cash.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+long size;
+struct LogData{
+	long logid;
+	char logdate[11];
+	char lognote[15];
+	double charge;
+	double balance;
+}; 
+
+int inputchoice()
+{
+	int mychoice;
+	printf("\nEnter your choice:\n");
+	printf("1-Add a new cash LOG.\n2-List All Cash LOG.\n"
+			"3-Query Last Cash LOG. \n0-End the program.\n");
+	scanf("%d", &mychoice);
+	return mychoice;
+}
+
+long getLogCount(FILE *cfptr)
+{
+	long begin, end, logcount;
+	fseek(cfptr, 0L, SEEK_SET);
+	begin = ftell(cfptr);
+	fseek(cfptr, 0, SEEK_END);
+	end = ftell(cfptr);
+	logcount = (end-begin) / size;
+	return logcount;
+}
+
+void ListAllLog(FILE *cfptr)
+{
+	struct LogData log;
+	fseek(cfptr, 0l, SEEK_SET);
+	fread(&log, size, 1, cfptr);
+	printf("logid logdate    lognote        charge    balance\n");
+	while( !feof(cfptr) )
+	{
+		printf("%-6ld%-11s%-15s%-10.2lf%-10.2lf\n", 
+				log.logid, log.logdate, log.lognote, log.charge, log.balance);
+		fread(&log, size, 1, cfptr);
+	}
+}
+
+void QueryLastLog(FILE *cfptr)
+{
+	struct LogData log;
+	long logcount;
+	logcount = getLogCount(cfptr);
+	if(logcount > 0)
+	{
+		fseek(cfptr, size*(logcount-1), SEEK_SET);
+		fread(&log, size, 1, cfptr);
+		printf("The last log is: \n");
+		printf("logid: %-6ld\nlogdate: %-11s\nlognote: %-15s\ncharge: %-10.2lf\nbalance: %-10.2lf\n", 
+				log.logid, log.logdate, log.lognote, log.charge, log.balance);
+	}
+	else printf("no logs in file! \n");
+} 
+
+void AddNewLog(FILE *cfptr)
+{
+	struct LogData log, lastlog;
+	long logcount;
+	printf("Input logdate(format:2016-01-01):");
+	scanf("%s", log.logdate);
+	printf("Input Charge: Income+ and expend-:");
+	scanf("%lf", &log.charge);
+	logcount = getLogCount(cfptr);
+	
+	if(logcount > 0)
+	{
+		fseek(cfptr, size*(logcount-1), SEEK_SET);
+		fread(&lastlog, size, 1, cfptr);
+		log.logid = lastlog.logid + 1;
+		log.balance = log.charge+lastlog.balance;
+	}
+	else 
+	{
+		log.logid = 1;
+		log.balance = log.charge;
+	}
+	rewind(cfptr);
+	printf("logid = %ld\n", log.logid);
+	fwrite(&log, sizeof(struct LogData), 1, cfptr);
+}
+
+FILE *openfile(char *openmode)
+{
+	FILE *fp;
+	if((fp = fopen("cashbox.dat", openmode)) == NULL)
+	{
+		printf("can not open file cashbox.dat!\n");
+		exit(0);
+	}
+	return fp;
+}
+
+int main(void)
+{
+	FILE *fp;
+	int choice;
+	size = sizeof(struct LogData);
+	while((choice = inputchoice()) != 0)
+	{
+		switch(choice)
+		{
+			case 1:
+				fp = openfile("ab+");
+				AddNewLog(fp); break;
+			case 2:
+				fp = openfile("rb");
+				ListAllLog(fp); break;
+			case 3:
+				fp = openfile("rb");
+				QueryLastLog(fp); break;
+			default: 
+				printf("Input Error."); break;
+		}
+		if(fclose(fp))
+		{
+			printf("Can not close the file!\n");
+			exit(0);
+		}
+	}
+	
+	return 0;
+}
